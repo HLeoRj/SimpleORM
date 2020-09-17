87b3a88a530644a1f92b4ee6b8a67f27df985dd7
Henrique Leonardo <hleorj@gmail.com>
2020-09-17 15:38:22 -0300

    [*]Ajustes para nova versao do SimpleOrm , com atribuitos de Storeprocedure

--- SimpleAttributes.pas
+++ SimpleAttributes.pas
@@ -25,10 +25,19 @@ type
   PK = class(TCustomAttribute)
   end;
 
+  FK = class(TCustomAttribute)
+  end;
+
+  NotNull = class(TCustomAttribute)
+  end;
+
   Ignore = class(TCustomAttribute)
   end;
 
   AutoInc = class(TCustomAttribute)
+  end;
+
+  NumberOnly = class(TCustomAttribute)
   end;
 
   Bind = class(TCustomAttribute)
@@ -36,19 +45,65 @@ type
     FField: String;
     procedure SetField(const Value: String);
   published
-    constructor Create(aField: String);
-    property Field: String read FField write SetField;
+    constructor Create (aField : String);
+    property Field : String read FField write SetField;
+  end;
+
+  Display = class(TCustomAttribute)
+  private
+    FName: string;
+  public
+    constructor Create(const aName: string);
+    property Name: string read FName write FName;
+  end;
+
+  Format = class(TCustomAttribute)
+  private
+    FMaxSize: integer;
+    FPrecision: integer;
+    FMask: string;
+    FMinSize: integer;
+  public
+    property MaxSize: integer read FMaxSize write FMaxSize;
+    property MinSize: integer read FMinSize write FMinSize;
+    property Precision: integer read FPrecision write FPrecision;
+    property Mask: string read FMask write FMask;
+    function GetNumericMask: string;
+    constructor Create(const aSize: Integer; const aPrecision: integer = 0); overload;
+    constructor Create(const aMask: string); overload;
+    constructor Create(const aRange: array of Integer); overload;
+  end;
+
+  Relationship = class abstract(TCustomAttribute)
+  private
+    FEntityName: string;
+  public
+    constructor Create(const aEntityName: string);
+    property EntityName: string read FEntityName write FEntityName;
+  end;
+
+  HasOne = class(Relationship)
+  end;
+
+  BelongsTo = class(Relationship)
+  end;
+  
+  HasMany = class(Relationship)
+  end;
+
+  BelongsToMany = class(Relationship)
   end;
 
   ProceName = class(TCustomAttribute)
   private
     FName: String;
+    procedure GetName(const Value: String);
   public
     constructor Create(Const aName: String);
-    property Name: String read FName write FName;
+    property Name: String read FName write GetName;
   end;
 
-  FieldResult= class(TCustomAttribute)
+  FieldResult = class(TCustomAttribute)
   private
     FName: String;
   public
@@ -90,6 +145,7 @@ type
 
 implementation
 
+
 { Bind }
 
 constructor Bind.Create(aField: String);
@@ -116,11 +172,60 @@ begin
   FName := aName;
 end;
 
+{ Display }
+
+constructor Display.Create(const aName: string);
+begin
+  FName := aName;
+end;
+
+{ Formato }
+
+constructor Format.Create(const aSize, aPrecision: integer);
+begin
+  FMaxSize := aSize;
+  FPrecision := aPrecision;
+end;
+
+constructor Format.Create(const aMask: string);
+begin
+  FMask := aMask;
+end;
+
+constructor Format.Create(const aRange: array of Integer);
+begin
+  FMinSize := aRange[0];
+  FMaxSize := aRange[High(aRange)];
+end;
+
+function Format.GetNumericMask: string;
+var
+  sTamanho, sPrecisao: string;
+begin
+  sTamanho := StringOfChar('0', FMaxSize - FPrecision);
+  sPrecisao := StringOfChar('0', FPrecision);
+
+  Result := sTamanho + '.' + sPrecisao;
+end;
+
+{ Relationship }
+
+constructor Relationship.Create(const aEntityName: string);
+begin
+  FEntityName := aEntityName;
+end;
+
+
 { ProceName }
 
 constructor ProceName.Create(const aName: String);
 begin
   FName := aName;
+end;
+
+procedure ProceName.GetName(const Value: String);
+begin
+  FName := Value;
 end;
 
 { ResultProc }
